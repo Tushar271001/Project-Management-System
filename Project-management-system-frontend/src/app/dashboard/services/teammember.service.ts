6c91406ccc60540d4956c1b0ed489d4a6870632d
Tushar <tytushar27@gmail.com>
2024-12-02 11:25:32 +0530

    files added

---
+++ Project-management-system-frontend/src/app/dashboard/services/teammember.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import BASE_URL from '../../auth/helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeammemberService {
+
+  constructor(private http:HttpClient) { }
+  
+  public addTeamMember(form:any):Observable<any>{
+    return this.http.post(BASE_URL +"/teammember/addTeamMember",form);
+  }
+
+  public getAllTeamMember():Observable<any>{
+    return this.http.get(BASE_URL +"/teammember/getAllTeamMember");
+  }
+  
+  public getAllDesignations():Observable<any>{
+    return this.http.get(BASE_URL +"/designation/getAllDesignations");
+  }
+
+  public updateTeamMember(formData: FormData): Observable<any> {
+    return this.http.patch(BASE_URL + '/teammember/updateTeamMember', formData);
+  }
+
+ 
+
+  public deleteTeamMember(teamMemberId: number): Observable<any> {
+    return this.http.delete(`${BASE_URL}/teammember/deleteTeamMember/${teamMemberId}`);
+  }
+
+
+}
